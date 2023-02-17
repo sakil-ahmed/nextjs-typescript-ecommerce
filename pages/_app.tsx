affc5cb8a454c5e9e4f749334fc882e2b61f6c3e
sakil-ahmed <sakilahmed1495@gmail.com>
2023-02-17 12:11:19 +0600

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Jost } from "@next/font/google";
+import type { AppProps } from "next/app";
+const jost = Jost({ subsets: ["latin"], variable: ["--font-jost"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={jost.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
